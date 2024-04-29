e37185159497979604a840d61ae31ed422a18959
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-04-29 14:49:21 -0400

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,49 +1,50 @@
 
-
-
-CREATE TABLE titles (
-    emp_title_id VARCHAR(50) NOT NULL,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY (emp_title_id)
+CREATE TABLE employees (
+    emp_no INT,
+    emp_title_id VARCHAR,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex CHAR(1) NOT NULL,  
+    hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, emp_title_id)
 );
 
 CREATE TABLE departments (
-    dept_no VARCHAR(50) NOT NULL,
-    dept_name VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY (dept_no)
-);
-
-CREATE TABLE employees (
-    emp_no INT NOT NULL,
-    emp_title_id VARCHAR(50) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    sex VARCHAR(1) NOT NULL,
-    hire_date DATE NOT NULL,
-    PRIMARY KEY (emp_no),
-    FOREIGN KEY (emp_title_id) REFERENCES titles(emp_title_id)
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
 );
 
 CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR(50) NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    emp_no INT,
+    emp_title_id VARCHAR, 
+    dept_no VARCHAR,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id),  
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(50) NOT NULL,
-    emp_no INT NOT NULL,
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    PRIMARY KEY (dept_no, emp_no)
+    emp_no INT,
+    emp_title_id VARCHAR,  
+    dept_no VARCHAR,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id),  
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE salaries (
-    emp_no INT NOT NULL,
+    emp_no INT,
+    emp_title_id VARCHAR,  
     salary INT NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, emp_title_id), 
+    FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id)  
+);
+
+CREATE TABLE titles (
+    emp_no INT,
+    emp_title_id VARCHAR,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY (emp_title_id, emp_no),
+    FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id)
 );
