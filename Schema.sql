007e8c670b8590edbdf61341e208bb54d0dcc19d
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-04-29 22:07:43 -0400

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,11 +1,7 @@
-CREATE TABLE employees (
-    emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR(20),  
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    sex CHAR(1) NOT NULL,  
-    hire_date DATE NOT NULL
+
+CREATE TABLE titles (
+    emp_title_id VARCHAR(20) PRIMARY KEY,
+    title VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE departments (
@@ -13,35 +9,42 @@ CREATE TABLE departments (
     dept_name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(20),  
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,  
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(emp_title_id)
+);
+
+CREATE TABLE salaries (
+    emp_no INT, 
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    PRIMARY KEY (emp_no, from_date),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
 CREATE TABLE dept_emp (
     emp_no INT, 
     dept_no VARCHAR(20),
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
     PRIMARY KEY (emp_no, dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),  
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
     emp_no INT,  
     dept_no VARCHAR(20),
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),  
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
-
-CREATE TABLE salaries (
-    emp_no INT PRIMARY KEY, 
-    salary INT NOT NULL  
-);
-
-CREATE TABLE titles (
-    emp_title_id VARCHAR(20) PRIMARY KEY,
-    title VARCHAR(100) NOT NULL
-);
-
--- ALTER Tables to reference Keys
-ALTER TABLE employees
-ADD FOREIGN KEY (emp_title_id) REFERENCES titles(emp_title_id);
-
-ALTER TABLE employees
-ADD FOREIGN KEY (emp_no) REFERENCES salaries(emp_no);
