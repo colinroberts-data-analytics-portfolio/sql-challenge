5e914442897c8b914577d343ac3a74ecd20640d7
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-04-30 13:58:07 -0400

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,20 +1,20 @@
 
 CREATE TABLE titles (
-    emp_title_id VARCHAR(20) PRIMARY KEY,
-    title VARCHAR(100) NOT NULL
+    emp_title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE departments (
-    dept_no VARCHAR(20) PRIMARY KEY,
-    dept_name VARCHAR(100) NOT NULL
+    dept_no VARCHAR(10) PRIMARY KEY,
+    dept_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR(20),  
+    emp_title_id VARCHAR(10),  
     birth_date DATE NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
     sex CHAR(1) NOT NULL,  
     hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(emp_title_id)
@@ -29,7 +29,7 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (
     emp_no INT, 
-    dept_no VARCHAR(20),
+    dept_no VARCHAR(10),
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -37,7 +37,7 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE dept_manager (
     emp_no INT,  
-    dept_no VARCHAR(20),
+    dept_no VARCHAR(10),
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
