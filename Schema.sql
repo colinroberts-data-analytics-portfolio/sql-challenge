8e08c32c78fd0bc038598c4a6d28590c886ec6b6
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-04-30 14:19:49 -0400

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -39,6 +39,6 @@ CREATE TABLE dept_manager (
     dept_no VARCHAR(10),  
     emp_no INT, 
     PRIMARY KEY (dept_no, emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)    
 );
